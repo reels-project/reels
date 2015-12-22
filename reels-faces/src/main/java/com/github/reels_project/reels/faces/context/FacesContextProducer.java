2dd120c4ad6963c1e23c72a146917e8d869329da
Takahiko Sato <t.sato@mosa-architect.com>
2015-12-22 17:19:33 +0900

    緊急対応 - FacesContextがInjectできない
    META-INF/beans.xmlにannotatedを付与。Producerにスコープアノテーションを付与したら動いた

--- reels-faces/src/main/java/com/github/reels_project/reels/faces/context/FacesContextProducer.java
+++ reels-faces/src/main/java/com/github/reels_project/reels/faces/context/FacesContextProducer.java
@@ -1,9 +1,11 @@
 package com.github.reels_project.reels.faces.context;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.faces.context.FacesContext;
 
+@ApplicationScoped
 public class FacesContextProducer {
 	
 	@Produces @RequestScoped
